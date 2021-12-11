d7e3e02507abd1781fbaaef95679710c08b3508e
Jamuwu <jamesguessis@gmail.com>
2021-12-11 18:08:27 -0500

    Nothing noteworthy

--- src/main.rs
+++ src/main.rs
@@ -59,10 +59,10 @@ fn main() -> Result<(), MyError> {
   window.set_icon(&icon);
 
   let mut canvas = window
-      .into_canvas()
-      .software()
-      .build()
-      .map_err(|e| e.to_string())?;
+    .into_canvas()
+    .software()
+    .build()
+    .map_err(|e| e.to_string())?;
   canvas.present();
   let texture_creator = canvas.texture_creator();
   let mut current_texture = DEFAULT.to_vec();
@@ -98,10 +98,14 @@ fn main() -> Result<(), MyError> {
   let mut dragging = None;
 
   let mut fps = FPSManager::new();
-  fps.set_framerate(60)?;
+  // Don't worry, your resources aren't actually being used much,
+  // Since only moving the window is using this framerate,
+  // Everything is drawn at most once every 100ms.
+  fps.set_framerate(120)?;
+
+  let mut time_since_last_draw = 0;
 
   'running: loop {
-    fps.delay();
     for event in events.poll_iter() {
       match event {
         Event::Quit { .. }
@@ -147,100 +151,107 @@ fn main() -> Result<(), MyError> {
         dragging = Some(events.mouse_state());
       }
       let mut point = POINT { x: 0, y: 0 };
-      unsafe { GetCursorPos(&mut point) };
-      let x = point.x - dragging.unwrap().x();
-      let y = point.y - dragging.unwrap().y();
-      canvas.window_mut().set_position(WindowPos::Positioned(x), WindowPos::Positioned(y));
+      // Should always work, but just in case
+      if unsafe { GetCursorPos(&mut point) } != 0 {
+        let x = point.x - dragging.unwrap().x();
+        let y = point.y - dragging.unwrap().y();
+        canvas.window_mut().set_position(WindowPos::Positioned(x), WindowPos::Positioned(y));
+      }
     }
 
     for frame in audio.try_iter() {
       device.queue(&frame);
     }
 
-    let queue_size = device.size() as i32;
-    let window = canvas.window_mut();
-    let title = if queue_size <= 2048 {
-      format!("Radio Hyrule ({})", queue_size)
-    } else if queue_size <= 2097152 { // 2048 * 1024
-      format!("Radio Hyrule ({}KB)", queue_size / 1024)
-    } else {
-      format!("Radio Hyrule ({:2}MB)", queue_size as f32 / 1048576.0)
-    };
-    window.set_title(&title).map_err(|e| e.to_string())?;
-
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-
-    canvas.set_draw_color(Color::RGB(123, 123, 123));
-    match updater.try_recv() {
-      Ok(song) => {
-        current = song.json.clone();
-        println!("Received: {:?}", song.json);
-        current_texture = match song.cover {
-          Some(bytes) => bytes,
-          None => DEFAULT.to_vec(),
-        };
-      },
-      Err(TryRecvError::Empty) => {},
-      _ => break,
-    }
-    let texture = texture_creator.load_texture_bytes(&current_texture)?;
-    canvas.copy(&texture, None, Rect::new(0, 0, 200, 200))?;
-
-    let surface = font
-      .render(if current.title.len() > 0 { &current.title } else { "Nothing playing" })
-      .blended_wrapped(Color::RGB(255, 255, 255), 350)
-      .map_err(|e| e.to_string())?;
-    let title = texture_creator
-      .create_texture_from_surface(&surface)
-      .map_err(|e| e.to_string())?;
-    let title_query = title.query();
-    canvas.copy(&title, None, Rect::new(225, 15, title_query.width, title_query.height))?;
-    if let Some(artist) = &current.artist {
-      if artist.len() > 0 {
-        let surface = small_font
-          .render(&format!("By {}", artist[0]))
-          .blended(Color::RGB(200, 200, 200))
-          .map_err(|e| e.to_string())?;
-        let artist = texture_creator
-          .create_texture_from_surface(&surface)
-          .map_err(|e| e.to_string())?;
-        let artist_query = artist.query();
-        canvas.copy(&artist, None, Rect::new(
-          225, 15 + title_query.height as i32,
-          artist_query.width, artist_query.height,
-        ))?;
-      }
-    }
-
-    let surface = small_font
-      .render(&format!("{} listeners", current.listeners))
-      .blended(Color::RGB(78, 78, 78))
-      .map_err(|e| e.to_string())?;
-    let title = texture_creator
-      .create_texture_from_surface(&surface)
-      .map_err(|e| e.to_string())?;
-    let TextureQuery { width, height, .. } = title.query();
-    canvas.copy(&title, None, Rect::new(225, 160, width, height))?;
+    if time_since_last_draw >= 100 {
+      let queue_size = device.size() as i32;
+      let window = canvas.window_mut();
+      let title = if queue_size <= 2048 {
+        format!("Radio Hyrule ({})", queue_size)
+      } else if queue_size <= 2097152 { // 2048 * 1024
+        format!("Radio Hyrule ({}KB)", queue_size / 1024)
+      } else {
+        format!("Radio Hyrule ({:2}MB)", queue_size as f32 / 1048576.0)
+      };
+      window.set_title(&title).map_err(|e| e.to_string())?;
   
-    canvas.set_draw_color(Color::RGB(255, 128, 128));
-    canvas.fill_rect(Rect::new(581, 1, 18, 18))?;
-    if *PLAYING.read().unwrap() {
-      canvas.box_(201, 190, 204, 200, Color::RGB(200, 200, 200))?;
-      canvas.box_(206, 190, 209, 200, Color::RGB(200, 200, 200))?;
+      canvas.set_draw_color(Color::RGB(0, 0, 0));
+      canvas.clear();
+  
+      canvas.set_draw_color(Color::RGB(123, 123, 123));
+      match updater.try_recv() {
+        Ok(song) => {
+          current = song.json.clone();
+          println!("Received: {:?}", song.json);
+          current_texture = match song.cover {
+            Some(bytes) => bytes,
+            None => DEFAULT.to_vec(),
+          };
+        },
+        Err(TryRecvError::Empty) => {},
+        _ => break,
+      }
+      let texture = texture_creator.load_texture_bytes(&current_texture)?;
+      canvas.copy(&texture, None, Rect::new(0, 0, 200, 200))?;
+  
+      let surface = font
+        .render(if current.title.len() > 0 { &current.title } else { "Nothing playing" })
+        .blended_wrapped(Color::RGB(255, 255, 255), 350)
+        .map_err(|e| e.to_string())?;
+      let title = texture_creator
+        .create_texture_from_surface(&surface)
+        .map_err(|e| e.to_string())?;
+      let title_query = title.query();
+      canvas.copy(&title, None, Rect::new(225, 15, title_query.width, title_query.height))?;
+      if let Some(artist) = &current.artist {
+        if artist.len() > 0 {
+          let surface = small_font
+            .render(&format!("By {}", artist[0]))
+            .blended(Color::RGB(200, 200, 200))
+            .map_err(|e| e.to_string())?;
+          let artist = texture_creator
+            .create_texture_from_surface(&surface)
+            .map_err(|e| e.to_string())?;
+          let artist_query = artist.query();
+          canvas.copy(&artist, None, Rect::new(
+            225, 15 + title_query.height as i32,
+            artist_query.width, artist_query.height,
+          ))?;
+        }
+      }
+  
+      let surface = small_font
+        .render(&format!("{} listeners", current.listeners))
+        .blended(Color::RGB(78, 78, 78))
+        .map_err(|e| e.to_string())?;
+      let title = texture_creator
+        .create_texture_from_surface(&surface)
+        .map_err(|e| e.to_string())?;
+      let TextureQuery { width, height, .. } = title.query();
+      canvas.copy(&title, None, Rect::new(225, 160, width, height))?;
+    
+      canvas.set_draw_color(Color::RGB(255, 128, 128));
+      canvas.fill_rect(Rect::new(581, 1, 18, 18))?;
+      if *PLAYING.read().unwrap() {
+        canvas.box_(201, 190, 204, 200, Color::RGB(200, 200, 200))?;
+        canvas.box_(206, 190, 209, 200, Color::RGB(200, 200, 200))?;
+      } else {
+        canvas.filled_trigon(202, 191, 202, 199, 208, 195, Color::RGB(200, 200, 200))?;
+      }
+      canvas.box_(211, 190, 600, 200, Color::RGB(128, 128, 128))?;
+      if let Some(duration) = current.duration {
+        let offset = *OFFSET.read().unwrap();
+        let end = current.started + duration as i64;
+        let length = end - current.started;
+        let now = Utc::now().timestamp_millis() / 1000;
+        let progress = (now - current.started - offset) as f64 / length as f64;
+        canvas.box_(211, 190, 211 + (progress * 390.0) as i16, 200, Color::RGB(225, 180, 36))?;
+      }
+      canvas.present();
+      time_since_last_draw = 0;
     } else {
-      canvas.filled_trigon(202, 191, 202, 199, 208, 195, Color::RGB(200, 200, 200))?;
+      time_since_last_draw += fps.delay();
     }
-    canvas.box_(211, 190, 600, 200, Color::RGB(128, 128, 128))?;
-    if let Some(duration) = current.duration {
-      let offset = *OFFSET.read().unwrap();
-      let end = current.started + duration as i64;
-      let length = end - current.started;
-      let now = Utc::now().timestamp_millis() / 1000;
-      let progress = (now - current.started - offset) as f64 / length as f64;
-      canvas.box_(211, 190, 211 + (progress * 390.0) as i16, 200, Color::RGB(225, 180, 36))?;
-    }
-    canvas.present();
   }
 
   Ok(())
@@ -309,14 +320,6 @@ fn song_updater(sender: Sender<SongUpdate>) -> Result<(), MyError> {
     let now = Utc::now().timestamp_millis();
     if now > next_epoch {
       let res = client.get(&format!("{}?_={}", url, now)).send()?;
-      /*
-      *OFFSET.write().unwrap() = if let Some(date) = res.headers().get("Date") {
-        let server = DateTime::parse_from_rfc2822(date.to_str().unwrap())?.timestamp_millis();
-        (now - server) / 1000
-      } else {
-        0
-      };
-      */
       current = res.json()?;
       if &current.title != &previous {
         current.info();
